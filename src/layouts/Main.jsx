b1e0971147d607d884ecd97787aa210c2390be8a
Faisal778 <62201274+Faisal778@users.noreply.github.com>
2024-08-17 02:22:39 +0600

    initial routes setup with main layout

---
+++ src/layouts/Main.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Main = () => {
+    return (
+        <div>
+            {/* Navbar  */}
+                <h1 className='text-3xl font-bold text-center'>Car Log</h1>
+            {/* Outlet  */}
+
+            {/* Footer  */}
+        </div>
+    );
+};
+
+export default Main;
