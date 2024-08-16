d4d2abb4ca8c18a68e18be094e481a8a55c02d22
Faisal778 <62201274+Faisal778@users.noreply.github.com>
2024-08-17 03:20:26 +0600

    firebase and AuthProvider setup complete

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCxk0xxnH5iCSnYSK14f9vCwWGznVEQiE4",
+  authDomain: "car-log-40ff4.firebaseapp.com",
+  projectId: "car-log-40ff4",
+  storageBucket: "car-log-40ff4.appspot.com",
+  messagingSenderId: "190598430343",
+  appId: "1:190598430343:web:fcd35479743cf640570222"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
