1ef3190c245a50b3dca2175e0ab076bd117fb42a
Faisal778 <62201274+Faisal778@users.noreply.github.com>
2024-08-17 16:34:56 +0600

    pagination added

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -9,6 +9,8 @@ const Products = () => {
     const [category, setCategory] = useState('');
     const [priceRange, setPriceRange] = useState('');
     const [sort, setSort] = useState('');
+    const [page, setPage] = useState(1); // Added page state
+    const [pageCount, setPageCount] = useState(0); // Added pageCount state
 
     const distinctBrands = [
         "Tesla", "Ford", "Chevrolet", "Nissan", "BMW", "Mercedes-Benz", 
@@ -29,94 +31,116 @@ const Products = () => {
             brandName,
             category,
             priceRange,
-            sort
+            sort,
+            page, // Include page number
+            limit: 10 // Limit results per page
         }).toString();
 
         try {
             const response = await fetch(`http://localhost:5000/cars?${params}`);
             const data = await response.json();
-            setCars(data);
-            if (data.length === 0){
+            setCars(data.cars); // Set cars data
+            setPageCount(data.totalPages); // Set total pages
+
+            if (data.cars.length === 0) {
                 Swal.fire({
                     position: "top-end",
                     icon: "error",
                     title: "No cars found",
                     showConfirmButton: false,
                     timer: 1500
-                  });
+                });
             }
         } catch (error) {
             console.error('Error fetching data:', error);
         }
     };
 
-    // Fetch initial data when the component mounts
     useEffect(() => {
         handleSearch();
-    }, []);
+    }, []); // Fetch data on changes
+
+    const handlePageChange = (newPage) => {
+        if (newPage > 0 && newPage <= pageCount) {
+            setPage(newPage); // Update page number
+        }
+    };
 
     return (
         <div>
-            <div className="lg:join w-[90%] mx-auto">
+            <div className="lg:join w-[90%] mx-auto my-4">
                 <div>
-                    <div>
-                        <input
-                            className="input input-bordered join-item rounded-md"
-                            placeholder="Search"
-                            value={search}
-                            onChange={(e) => setSearch(e.target.value)}
-                        />
+                    <input
+                        className="input input-bordered join-item rounded-md"
+                        placeholder="Search"
+                        value={search}
+                        onChange={(e) => setSearch(e.target.value)}
+                    />
+                    <select
+                        className="select select-bordered join-item"
+                        value={brandName}
+                        onChange={(e) => setBrand(e.target.value)}
+                    >
+                        <option value="">Brand</option>
+                        {distinctBrands.map(brand => (
+                            <option key={brand} value={brand}>{brand}</option>
+                        ))}
+                    </select>
+                    <select
+                        className="select select-bordered join-item"
+                        value={category}
+                        onChange={(e) => setCategory(e.target.value)}
+                    >
+                        <option value="">Category</option>
+                        {distinctCategories.map(category => (
+                            <option key={category} value={category}>{category}</option>
+                        ))}
+                    </select>
+                    <select
+                        className="select select-bordered join-item"
+                        value={priceRange}
+                        onChange={(e) => setPriceRange(e.target.value)}
+                    >
+                        <option value="">Price Range</option>
+                        <option value="below-50000">Below $50,000</option>
+                        <option value="50000-70000">$50,000 - $70,000</option>
+                        <option value="above-70000">Above $70,000</option>
+                    </select>
+                    <select
+                        className="select select-bordered join-item"
+                        value={sort}
+                        onChange={(e) => setSort(e.target.value)}
+                    >
+                        <option value="">Sort</option>
+                        <option value="price-low-high">Price Low to High</option>
+                        <option value="price-high-low">Price High to Low</option>
+                        <option value="newest-first">Newest First</option>
+                        <option value="oldest-first">Oldest First</option>
+                    </select>
+                    <div className="indicator">
+                        <button className="btn join-item" onClick={handleSearch}>Search</button>
                     </div>
-                </div>
-                <select
-                    className="select select-bordered join-item"
-                    value={brandName}
-                    onChange={(e) => setBrand(e.target.value)}
-                >
-                    <option value="">Brand</option>
-                    {distinctBrands.map(brand => (
-                        <option key={brand} value={brand}>{brand}</option>
-                    ))}
-                </select>
-                <select
-                    className="select select-bordered join-item"
-                    value={category}
-                    onChange={(e) => setCategory(e.target.value)}
-                >
-                    <option value="">Category</option>
-                    {distinctCategories.map(category => (
-                        <option key={category} value={category}>{category}</option>
-                    ))}
-                </select>
-                <select
-                    className="select select-bordered join-item"
-                    value={priceRange}
-                    onChange={(e) => setPriceRange(e.target.value)}
-                >
-                    <option value="">Price Range</option>
-                    <option value="below-50000">Below $50,000</option>
-                    <option value="50000-70000">$50,000 - $70,000</option>
-                    <option value="above-70000">Above $70,000</option>
-                </select>
-                <select
-                    className="select select-bordered join-item"
-                    value={sort}
-                    onChange={(e) => setSort(e.target.value)}
-                >
-                    <option value="">Sort</option>
-                    <option value="price-low-high">Price Low to High</option>
-                    <option value="price-high-low">Price High to Low</option>
-                    <option value="newest-first">Newest First</option>
-                    <option value="oldest-first">Oldest First</option>
-                </select>
-                <div className="indicator">
-                    <span className="indicator-item badge badge-secondary">new</span>
-                    <button className="btn join-item" onClick={handleSearch}>Search</button>
                 </div>
             </div>
 
-            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4'>
-                {cars.map(car => (<CarCard key={car._id} car={car}></CarCard>))}
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 mt-4'>
+                {cars.map(car => (<CarCard key={car._id} car={car} />))}
+            </div>
+
+            <div className="pagination">
+                <button
+                    onClick={() => handlePageChange(page - 1)}
+                    disabled={page === 1} // Disable if on the first page
+                >
+                    Previous
+                </button>
+                <span>Page {page} of {pageCount}</span>
+                <button
+                    onClick={() => handlePageChange(page + 1)}
+                    disabled={page === pageCount} // Disable if on the last page
+                >
+                    Next
+                </button>
             </div>
         </div>
     );
